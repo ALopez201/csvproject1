d73a1864441d885f331825aa1c2c0f81b3a21e14
ALopez201 <al234@njit.edu>
2019-07-05 01:09:52 -0400

    added records class.

--- public/index.php
+++ public/index.php
@@ -1,21 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<div class="container">
+    <div class="jumErbotron">
+        <h1>Anthony's CSV File Web Page</h1>
+        <p>This page is intended to disply the resulting table after a CSV file has been read as an array
+        then used to generate an html table.</p>
+    </div>
+
+
+
+
 <?php
 
-main::start();
+main::start("SalesJan2009.csv");
 
 class main {
-    static public function start()
-    {
-        $file = fopen("SalesJan2009.csv", "r");
-        while(!feof($file))
 
+    static public function start($filename) {
+
+
+        $records = csv::getRecords($filename);
+
+        print_r($records);
+
+    }
+
+}
+
+
+
+
+class csv {
+    /**
+     * @param $filename
+     * @return array
+     */
+    static public function getRecords($filename) {
+
+        $file = fopen($filename, "r");
+
+        while(! feof($file))
         {
+
             $record = fgetcsv($file);
 
             $records[] = $record;
+
         }
 
         fclose($file);
-        print_r($records);
+        return $records;
+
+    }
+
+}
+
+class record{}
+
+class recordfactory {
+
+    public static function create(Array $array = null) {
+
+        $record = new record();
+
+        return $record;
 
     }
 }
